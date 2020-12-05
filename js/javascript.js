dc425a03f51e9f0b19d22cb46e7fb2e41fc6d9f0
danielgroen <groendaniel@gmail.com>
2020-12-05 11:42:56 +0100

    Updates

--- js/javascript.js
+++ js/javascript.js
@@ -1,23 +1,23 @@
-import bodyClass from './source/bodyclass.js';
-import breadcrumbs from './source/breadcrumb.js';
-import formvalidator from './source/formvalidator.js';
-import fullscreen from './source/fullscreen.js';
-import gridVisualizer from './source/grid-visualizer.js';
-import jsonloader from './source/jsonloader.js';
-import lazyload from './source/lazyload.js';
-import owlcarousel from './source/owlcarousel.js';
-import prices from './source/prices.js';
-import spamspan from './source/spamspan.js';
+import bodyClass from "./source/bodyclass.js";
+import breadcrumbs from "./source/breadcrumb.js";
+import formvalidator from "./source/formvalidator.js";
+import fullscreen from "./source/fullscreen.js";
+import gridVisualizer from "./source/grid-visualizer.js";
+import jsonloader from "./source/jsonloader.js";
+import lazyload from "./source/lazyload.js";
+import owlcarousel from "./source/owlcarousel.js";
+import prices from "./source/prices.js";
+import form from "./source/form.js";
 
-window.addEventListener('load', async () => {
+window.addEventListener("load", async () => {
   bodyClass();
   breadcrumbs();
   formvalidator();
   gridVisualizer();
   jsonloader();
-  await lazyload();
+  form();
   owlcarousel();
+  await lazyload();
   prices();
-  spamspan();
   fullscreen();
 });
--- js/source/formvalidator.js
+++ js/source/formvalidator.js
@@ -1,24 +1,21 @@
-export default function() {
-
+export default function () {
   var invalid = 0;
   function validateForm() {
-      $('.form-field').each(function () {
-          if ($(this).val() == '') {
-              invalid++;
-          }
-      });
+    $(".form-field").each(function () {
+      if ($(this).val() == "") {
+        invalid++;
+      }
+    });
 
-
-    if ( invalid > 0) {
-        invalid = 0;
-        $('input[type="submit"').removeClass('validated');
-    }
-    else {
-      $('input[type="submit"').addClass('validated');
+    if (invalid > 0) {
+      invalid = 0;
+      $('input[type="submit"').removeClass("validated");
+    } else {
+      $('input[type="submit"').addClass("validated");
     }
   }
 
-  $( ".form-field" ).on('keydown',function(e) {
+  $(".form-field").on("keydown", function (e) {
     validateForm();
   });
 }
