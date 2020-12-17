11d8efabedd0afdb45f13ec3f68c2478d70cb7d9
danielgroen <groendaniel@gmail.com>
2020-12-17 21:21:09 +0100

    Updates

--- js/source/form.js
+++ js/source/form.js
@@ -15,55 +15,39 @@ export default function () {
   var $contactForm = $(".contactform");
 
   $(".send").on("click", function (e) {
-    console.log("aaa");
     $(".load-wrapper").addClass("visible");
     $(".overtake").addClass("visible");
 
-    var name = $("#name").val(),
-      subject = $("#subject").val(),
-      email = $("#mail").val(),
-      message = $("#message").val();
-
     if ($("#email").val()) return false; // honeypot
-    let url = window.location.href;
 
-    var sendInfo = {
-      Name: name,
-      Subject: subject,
-      Email: email,
-      Message: message,
+    const body = {
+      name: $("#name").val(),
+      subject: $("#subject").val(),
+      email: $("#mail").val(),
+      honeypot: $("#email").val(),
+      message: $("#message").val(),
     };
     let mailHandler;
-    if (!url.includes("localhost")) {
+    if (!window.location.href.includes("localhost")) {
       mailHandler = "https://mailhandler.danielgroen.nl";
     } else {
       mailHandler = "http://mailhandler.test";
     }
 
     e.preventDefault();
-    $.ajax({
-      url: mailHandler,
-      method: "POST",
-      data: { studioginger: JSON.stringify(sendInfo) },
-      dataType: "json",
-      beforeSend: function () {
+    axios
+      .post(mailHandler, { studioginger: body })
+      .then((res) => {
         $(".load-wrapper").removeClass("visible");
         $(".thankyou").addClass("visible");
         $(".thankyou .send, .overtake").on("click touch", function () {
           location.reload();
         });
-      },
-      success: function (data) {
         console.log("success");
-      },
-      error: function (xhr, status, error) {
-        var errorMessage = xhr.status + ": " + xhr.statusText;
-        console.log(errorMessage);
-
-        // $contactForm.append(
-        //   '<div class="alert alert--error">Oops, an error occurred.</div>'
-        // );
-      },
-    });
+      })
+      .catch((err) => {
+        console.log(err);
+        // catch error
+      });
   });
 }
